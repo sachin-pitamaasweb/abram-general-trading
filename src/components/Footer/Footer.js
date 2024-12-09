e77e198136be9886887bd9146c3bec462a049ef7
sachin.pitamaasweb <165882071+sachin-pitamaasweb@users.noreply.github.com>
2024-12-09 11:52:05 +0530

    new updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-// import { Mail, Phone, MapPin } from "lucide-react";
 
 import logo from "../../app/logo.png";
 
