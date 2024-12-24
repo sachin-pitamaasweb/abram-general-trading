ab561bc34c5f2563ac2b3940080b887f74056801
sachin.pitamaasweb <165882071+sachin-pitamaasweb@users.noreply.github.com>
2024-12-24 15:24:47 +0530

    new updates

--- src/components/HeroCarousel/HeroCarousel.js
+++ src/components/HeroCarousel/HeroCarousel.js
@@ -12,16 +12,28 @@ export default function HeroCarousel() {
 
     const images = [
         {
-            src: "/images/banner/b-1.png",
+            src: "/images/home/banner/b-1.png",
             alt: "Aerial view of agricultural fields showing farmers planting crops",
         },
         {
-            src: "/images/banner/b-1.png",
+            src: "/images/home/banner/b-2.png",
             alt: "Green paddy fields",
         },
         {
-            src: "/images/banner/b-1.png",
+            src: "/images/home/banner/b-3.png",
             alt: "Harvesting wheat field",
+        },
+        {
+            src: "/images/home/banner/b-4.png",
+            alt: "Aerial view of agricultural fields showing farmers planting crops",
+        },
+        {
+            src: "/images/home/banner/b-5.png",
+            alt: "Aerial view of agricultural fields showing farmers planting crops",
+        },
+        {
+            src: "/images/home/banner/b-6.png",
+            alt: "Aerial view of agricultural fields showing farmers planting crops",
         },
     ];
 
