492db97714799b142447da82f8cd9ec6881c3fc5
sachin.pitamaasweb <165882071+sachin-pitamaasweb@users.noreply.github.com>
2025-01-24 17:36:12 +0530

    Updated ProductShowcase, PunjabKitchen, and Zehnab components with aligned hover animations, dynamic height adjustments, and smooth transitions.

--- src/app/page.js
+++ src/app/page.js
@@ -11,32 +11,81 @@ import Infrastructure from "@/components/Infrastructure/Infrastructure";
 import Testimonials from "@/components/Testimonials/Testimonials";
 import ServicesSection from "@/components/ServicesSection/ServicesSection";
 import CardGrid from "@/components/CardGrid/CardGrid";
+import ProductShowcase from "@/components/ProductShowcase/ProductShowcase";
+import PunjabKitchen from "@/components/PunjabKitchen/PunjabKitchen";
+import Zehnab from "@/components/Zehnab/Zehnab";
 
 export default function Home() {
-const images = [
-        {
-            src: "/images/home/banner/home.png",
-            alt: "Aerial view of agricultural fields showing farmers planting crops",
-        },
-    ];
+  const images = [
+    {
+      src: "/images/home/banner/home.png",
+      alt: "Aerial view of agricultural fields showing farmers planting crops",
+    },
+  ];
+
+  const additionalImagesForPunjab = [
+    "https://res.cloudinary.com/dtivafy25/image/upload/v1737709736/uhlkuk_1_oubkrg.png",
+    "https://res.cloudinary.com/dtivafy25/image/upload/v1737709735/287_c6r2ag.png",
+  ];
+
+  const additionalImages = [
+    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/292-3N5HVBHWlTvjp90jm8PXxthhj4CWFw.png",
+    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/288-06msZ9cT5lCwLII7rlvdTjKMtOtYhX.png",
+    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/290-HUFjz10t5mFMtM479ovCZYuqKBJ0V3.png",
+    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/291-lacqDIcsGp0Vzs3wjxF98KDrffSvI2.png",
+  ];
+
+  const additionalImagesForZehnab = [
+    "https://res.cloudinary.com/dtivafy25/image/upload/v1737709734/295_spap4o.png",
+    "https://res.cloudinary.com/dtivafy25/image/upload/v1737709733/294_n8l0nj.png",
+  ];
 
   return (
     <>
-    <HeroCarousel
-      images={images}
-    />
-    <WelcomeSection />
-    {/* <ServicesSection /> */}
-    <CardGrid />
-    {/* <Founders /> */}
-    <Milestones />
-    {/* <BrandMarquee /> */}
-    {/* <ProductRange /> */}
-    <Infrastructure />
-    <PresenceMap />
-    {/* <BannerSection /> */}
-    <Gallery />
-    <Testimonials />
+      <HeroCarousel
+        images={images}
+      />
+      <div className="container mx-auto p-12">
+        <div className="max-w-5xl mx-auto" style={{ maxWidth: "90rem" }}>
+          <PunjabKitchen
+            mainImage="https://res.cloudinary.com/dtivafy25/image/upload/v1737709738/924_fk0uix.png"
+            additionalImages={additionalImagesForPunjab}
+            title="Punjab Kitchen"
+            bgGradient="linear-gradient(120deg, #000000, #3E403F 35%, #F74731 100%)"
+          />
+        </div>
+      </div>
+      <div className="container mx-auto p-12">
+        <div className="max-w-5xl mx-auto" style={{ maxWidth: "90rem" }}>
+          <ProductShowcase
+            mainImage="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/923-Cyx2G6dVFzyTYE4sklNz6277Dy6eDD.png"
+            additionalImages={additionalImages}
+            bgGradient="linear-gradient(90deg, #FFFFFF -5%, #2384F6 100%, #52A2FF 45%)"
+          />
+        </div>
+      </div>
+      <div className="container mx-auto p-12">
+        <div className="max-w-5xl mx-auto" style={{ maxWidth: "90rem" }}>
+          <Zehnab
+            mainImage="https://res.cloudinary.com/dtivafy25/image/upload/v1737709734/921_ybq4b7.png"
+            additionalImages={additionalImagesForZehnab}
+            title="Zehnab"
+            bgGradient="linear-gradient(90deg, #FFF578 -15%, #35BE95 100%, #35BE95 45%)"
+          />
+        </div>
+      </div>
+      <WelcomeSection />
+      {/* <ServicesSection /> */}
+      <CardGrid />
+      {/* <Founders /> */}
+      <Milestones />
+      {/* <BrandMarquee /> */}
+      {/* <ProductRange /> */}
+      <Infrastructure />
+      <PresenceMap />
+      {/* <BannerSection /> */}
+      <Gallery />
+      <Testimonials />
     </>
   );
 }
